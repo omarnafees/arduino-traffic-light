886afc69e19c7206a386a1fb90a5f80f72a28f97
Omar <o.nafees7@gmail.com>
2026-09-05 01:35:30 +0300

    Create traffic_light.ino

---
+++ traffic_light.ino
@@ -0,0 +1,87 @@
+  int RedLED=3;
+  int YellowLED=10;
+  int GreenLED=6;
+
+void setup() {
+
+  pinMode (3,OUTPUT);
+  pinMode (10,OUTPUT);
+  pinMode (6,OUTPUT);
+}
+
+void loop() {
+  digitalWrite (RedLED,HIGH);
+  digitalWrite (YellowLED,LOW);
+  digitalWrite (GreenLED,LOW);
+  delay (5000);
+
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,HIGH);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,LOW);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,HIGH);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,LOW);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+   digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,HIGH);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+ digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,LOW);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,LOW);
+  digitalWrite (GreenLED,HIGH);
+  delay (5000);
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,HIGH);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,LOW);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,HIGH);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,LOW);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,HIGH);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+  digitalWrite (RedLED,LOW);
+  digitalWrite (YellowLED,LOW);
+  digitalWrite (GreenLED,LOW);
+  delay (500);
+
+  
+}
